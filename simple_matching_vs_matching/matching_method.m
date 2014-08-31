9dfcf88a25c63e67376d674dda169abdde083f4c
Jonathan Grizou <jonathan.grizou@inria.fr>
2014-08-31 13:44:12 -0500

    simple vs matching - artificial data

---
+++ simple_matching_vs_matching/matching_method.m
@@ -0,0 +1,7 @@
+methodInfo = struct;
+methodInfo.classifierMethod = 'online';
+methodInfo.samplingMethod = 'one_shot';
+methodInfo.estimateMethod = 'matching';
+methodInfo.cumulMethod = 'filter';
+methodInfo.probaMethod = 'pairwise';
+rec.log_field('methodInfo', methodInfo)
---
+++ simple_matching_vs_matching/simple_matching_method.m
@@ -0,0 +1,7 @@
+methodInfo = struct;
+methodInfo.classifierMethod = 'online';
+methodInfo.samplingMethod = 'one_shot';
+methodInfo.estimateMethod = 'simple_matching';
+methodInfo.cumulMethod = 'filter';
+methodInfo.probaMethod = 'pairwise';
+rec.log_field('methodInfo', methodInfo)
